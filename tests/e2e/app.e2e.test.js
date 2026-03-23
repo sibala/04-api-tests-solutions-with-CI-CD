594e41dfa799a6c25574b1ae667e3b3ce8d4c83f
Sibar Al-ani <sibar.alani@gmail.com>
2026-03-23 14:06:40 +0100

    e2e done :)

--- tests/e2e/app.e2e.test.js
+++ tests/e2e/app.e2e.test.js
@@ -1,7 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("full flow: load posts and tags on start", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("/");
+});
+
+
+test("full flow: load posts and tags on start", async ({ page }) => {
+  // await page.goto("/");
 
   //   same concept as document.querySelectorAll('.post-card:first-child')
   await expect(page.locator(".post-card").first()).toBeVisible();
@@ -11,3 +16,21 @@ test("full flow: load posts and tags on start", async ({ page }) => {
   await expect(options.first()).toHaveText("Alla");
   expect(await options.count()).toBeGreaterThan(1);
 });
+
+
+test("Changing the tag dropdown filters the posts (verify that shown posts contain the selected tag)", async ({ page }) => {
+  // await page.goto("/");
+
+  //   same concept as document.querySelectorAll('.post-card:first-child')
+  await expect(page.locator(".post-card").first()).toBeVisible();
+  await page.selectOption("#tag-filter", "history");
+  await expect(page.locator(".post-card").first()).toBeVisible();
+
+  let tagText = await page.locator(".post-meta").first().textContent()
+  expect(tagText).toContain("history")
+
+  tagText = await page.locator(".post-meta").nth(10).textContent() // the 11th child
+  expect(tagText).toContain("history")
+});
+
+
